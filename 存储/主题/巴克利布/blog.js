63450e2e291428db67d3171154df28f6183c4873
aedelnz <44128582+aedelnz@users.noreply.github.com>
2022-01-27 21:40:02 +0800

    Create blog.js

---
+++ 存储/主题/巴克利布/blog.js
@@ -0,0 +1,7 @@
+document.getElementById("home").innerHTML="<div class='elec'><a href='/JIXIEJIDIGUAN' title='爱发电'><div class='elec-trigger'><span class='elec-trigger-icon icon'></span>为TA充电</div></a></div><a href='https://jixiejidiguan.top'>首页</a><a href='https://jixiejidiguan.top/A2zml/'>导航站</a><style>.elec .elec-trigger-icon{background-position:-278px -918px;width:20px;height:24px;vertical-align:middle;margin-right:12px}.icon{display:inline-block;background-image:url(//s1.hdslb.com/bfs/static/jinkela/space/assets/icons.png)}.elec .elec-trigger{background:#f25d8e;border-radius:6px;box-shadow:0 4px 4px rgba(255,112,159,.3);color:#fff;cursor:pointer;display:inline-block;font-size:18px;line-height:50px;padding:0 24px}</style>"
+
+document.write("<link rel=\"stylesheet\" href=\"\/\/jixiejidiguan.top\/stalye\/index.css\" >");
+document.write("<link rel=\"stylesheet\" href=\"\/\/jixiejidiguan.top\/stalye\/mdui\/css\/mdui.css\" >");
+document.write("<script src=\"\/\/jixiejidiguan.top\/stalye\/mdui\/js\/mdui.js\"><\/script>");
+document.write("<br><script src=\"\/\/jixiejidiguan.top\/stalye\/js\/zhucx.js\"><\/script>");
+document.write("<script src=\"https:\/\/code.jquery.com\/jquery-3.0.0.min.js\"><\/script>");
