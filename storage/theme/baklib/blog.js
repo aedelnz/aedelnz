5731e6fb9b033049fb264bda726b320f971014de
aedelnz <44128582+aedelnz@users.noreply.github.com>
2022-08-17 16:36:57 +0800

    Update blog.js

--- storage/theme/baklib/blog.js
+++ storage/theme/baklib/blog.js
@@ -1,6 +1,8 @@
 document.getElementById("home").innerHTML="<div class='elec'><a href='https://jixiejidiguan.top/JIXIEJIDIGUAN' title='爱发电'><div class='elec-trigger'><span class='elec-trigger-icon icon'></span>为TA充电</div></a></div><a href='https://jixiejidiguan.top'>首页</a><a href='https://jixiejidiguan.top/A2zml/'>导航站</a><style>.elec .elec-trigger-icon{background-position:-278px -918px;width:20px;height:24px;vertical-align:middle;margin-right:12px}.icon{display:inline-block;background-image:url(//s1.hdslb.com/bfs/static/jinkela/space/assets/icons.png)}.elec .elec-trigger{background:#f25d8e;border-radius:6px;box-shadow:0 4px 4px rgba(255,112,159,.3);color:#fff;cursor:pointer;display:inline-block;font-size:18px;line-height:50px;padding:0 24px}</style>"
-document.write("<style>header{border-radius:0px;background-color:#fff0;padding:0px 0}.search-query{width:100%;border-radius:8px;background-color:#fff0;padding:8px 0}#docsSearch h1{width:100%;border-radius:8px;background-color:#fff0;padding:8px 0}<\/style>");
 
-document.write("<script src=\"https:\/\/jixiejidiguan.top\/aedelnz\/MN\/role-anim.js\"><\/script>");
+
+document.write("<link rel=\"stylesheet\" href=\"https:\/\/jixiejidiguan.top\/start\/mdui\/css\/mdui.min.css?v=1.0\" \/>");
+document.write("<script src=\"https:\/\/jixiejidiguan.top\/syart\/mdui\/js\/mdui.min.js?v=1.0\"><\/script>");
+document.write("<script src=\"https:\/\/jixiejidiguan.top\/start\/jquery.min.js?v=1.0\"><\/script>");
 document.write("<link rel=\"stylesheet\" href=\"https:\/\/jixiejidiguan.top\/start\/index.css?v=1.0\" >");
 document.write("<script src=\"\/\/jixiejidiguan.top\/start\/origin.js\"><\/script>");
